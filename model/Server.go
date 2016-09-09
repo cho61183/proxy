759037437ee895e295f7ef7cf2e8b961d94dc731
瓜子 <61183@qq.com>
2016-09-09 19:42:35 +0800

    分层开发

---
+++ model/Server.go
@@ -0,0 +1,52 @@
+package model
+
+import (
+	"fmt"
+	"strings"
+)
+
+/*
+	定义接收的参数.
+	action 动作
+	model 模块
+	ip ip地址
+	port 端口
+*/
+type ServerInfo struct {
+	Action string
+	Model  string
+	Ip     string
+	Port   int
+}
+
+/*
+ 对服务的分发动作处理。
+add
+*/
+func (s ServerInfo) Controller() (string, bool) {
+	fmt.Println("This is Model.Servier msg :")
+	switch strings.ToLower(s.Action) {
+	case "add":
+		return add(s)
+	case "del":
+		return del(s)
+	}
+	return "ok", true
+
+}
+
+/*
+添加动作
+*/
+func add(s ServerInfo) (string, bool) {
+	fmt.Println("add")
+	return "This add:", true
+}
+
+/*
+删除动作
+*/
+func del(s ServerInfo) (string, bool) {
+	fmt.Println("del")
+	return "This del:", true
+}
--- proxy.go
+++ proxy.go
@@ -4,39 +4,21 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/cho61183/proxy/model"
 	"github.com/cho61183/proxy/tools"
 )
 
-/*
-	定义接收的参数.
-	action 动作
-	model 模块
-	ip ip地址
-	port 端口
-*/
-type ServerInfo struct {
-	Action string
-	Model  string
-	Ip     string
-	Port   int
-}
-
 func main() {
-
 	if msg, ok := tools.CheckArgs(os.Args); !ok {
 		fmt.Println(msg)
 		return
 	}
 
-	Info := ServerInfo{}
+	Info := model.ServerInfo{}
 
 	Info.Action = os.Args[1]
 	Info.Model = os.Args[2]
 	Info.Ip = os.Args[3]
 	Info.Port = tools.ToInt(os.Args[4])
-
-	fmt.Println(Info)
-	//	switch ServerInfo.Action {
-
-	//	}
+	Info.Controller()
 }
